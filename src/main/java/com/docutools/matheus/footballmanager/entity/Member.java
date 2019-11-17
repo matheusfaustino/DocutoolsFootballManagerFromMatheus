11d470be5aff7f02e54edc0fbee98d6385dea3c8
Matheus Faustino <matheuspfaustino@gmail.com>
2019-11-17 14:38:52 -0300

    Adds entity and repository for member table

---
+++ src/main/java/com/docutools/matheus/footballmanager/entity/Member.java
@@ -0,0 +1,32 @@
+package com.docutools.matheus.footballmanager.entity;
+
+import com.docutools.matheus.footballmanager.roles.TeamRoles;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+/**
+ * I had to put quotation marks on the name because it is a reserved word in mysql 8.0.18 (docker current mysql version)
+ */
+@Table(name = "\"member\"")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue
+	@Setter(AccessLevel.PROTECTED)
+    private UUID memberId;
+
+    @Column()
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private TeamRoles role;
+}
