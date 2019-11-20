aff9ce7f58eb2eab2b931e78c27909b049bb07ab
Matheus Faustino <matheuspfaustino@gmail.com>
2019-11-19 23:26:21 -0300

    Changes https status of the exceptions to bad request

--- src/main/java/com/docutools/matheus/footballmanager/exception/MaximumDoctorReachedException.java
+++ src/main/java/com/docutools/matheus/footballmanager/exception/MaximumDoctorReachedException.java
@@ -3,6 +3,6 @@ package com.docutools.matheus.footballmanager.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class MaximumDoctorReachedException extends RuntimeException {
 }
