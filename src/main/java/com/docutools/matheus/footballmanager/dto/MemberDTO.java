270e1f947dd1941820805ae31a6b37a44f34b7fc
Matheus Faustino <matheuspfaustino@gmail.com>
2019-11-20 20:44:58 -0300

    Removes firstTeam and benched field from non-players members

--- src/main/java/com/docutools/matheus/footballmanager/dto/MemberDTO.java
+++ src/main/java/com/docutools/matheus/footballmanager/dto/MemberDTO.java
@@ -1,10 +1,13 @@
 package com.docutools.matheus.footballmanager.dto;
 
 import com.docutools.matheus.footballmanager.entity.Member;
+import com.docutools.matheus.footballmanager.role.TeamRoles;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Getter
@@ -13,10 +16,18 @@ public class MemberDTO {
 	private UUID memberId;
 	private String name;
 	private String role;
-	private Boolean firstTeam;
-	private Boolean benched;
+
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private Optional<Boolean> firstTeam;
+	@JsonInclude(JsonInclude.Include.NON_NULL)
+	private Optional<Boolean> benched;
 
 	public static MemberDTO convertToDto(Member member) {
-		return new MemberDTO(member.getMemberId(), member.getName(), member.getRole().getLabel(), member.getFirstTeam(), member.getBenched());
+		boolean isPlayer = member.getRole().getParentId().getLabel().equalsIgnoreCase(TeamRoles.PLAYER.name());
+		if (isPlayer) {
+			return new MemberDTO(member.getMemberId(), member.getName(), member.getRole().getLabel(), Optional.of(member.getFirstTeam()), Optional.of(member.getBenched()));
+		} else {
+			return new MemberDTO(member.getMemberId(), member.getName(), member.getRole().getLabel(), null, null);
+		}
 	}
 }
